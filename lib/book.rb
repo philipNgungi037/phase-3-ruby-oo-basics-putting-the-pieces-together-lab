c04fb61568a55461e64e27b653829ac5faf20c71
PHILIP <philophilo037@gmail.com>
2022-10-29 11:31:46 +0300

    Completed assignment

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_reader :title
+    attr_accessor :author, :page_count, :genre
+
+    def initialize(title="And Then There Were None", author="Agatha Christie",genre)
+        @title = title
+        @author = author
+        @genre = genre
+
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
 
+end
